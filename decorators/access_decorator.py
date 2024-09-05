32621e89d5242ad87f8f14a77d356494d1b6b0c5
painkillergodsewostyanow <thebladethatbringsdeath@gmail.com>
2024-09-05 15:56:54 +0300

    small fix

--- decorators/access_decorator.py
+++ decorators/access_decorator.py
@@ -2,7 +2,6 @@ from functools import wraps
 from typing import Callable
 
 from django.http import HttpRequest, HttpResponse
-from django.urls.exceptions import Http404
 
 
 def right_required(condition: Callable[[HttpRequest], bool], if_hasnt: Callable):
@@ -11,7 +10,7 @@ def right_required(condition: Callable[[HttpRequest], bool], if_hasnt: Callable)
         def _wrapped_view(request: HttpRequest, *args, **kwargs):
             if condition(request, *args, **kwargs):
                 return view_func(request, *args, **kwargs)
-            if_hasnt(request)  # Действие если проверка не выполнилась
+            return if_hasnt(request)  # Действие если проверка не выполнилась
 
         return _wrapped_view
 
